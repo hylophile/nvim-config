f3d0d03a5d14e0b9e11482c9f5d4ed09d89447f5
hylo <hylo@posteo.de>
2023-03-20 16:32:01 +0100

    feat: help window on the side

--- init.lua
+++ init.lua
@@ -636,5 +636,7 @@ vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
   end,
 })
 
+vim.cmd [[autocmd FileType help wincmd L]]
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
