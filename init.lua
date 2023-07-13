eae1e4a50afeb541d3df0e0e49f011902da01089
hylo <hylo@posteo.de>
2023-07-13 20:19:11 +0200

    feat: neovide transparency

--- init.lua
+++ init.lua
@@ -649,6 +649,7 @@ if vim.g.neovide then
   vim.o.guifont = 'Agave:h14' -- text below applies for VimScript
   vim.g.neovide_cursor_vfx_mode = 'torpedo'
   -- vim.g.neovide_cursor_vfx_particle_lifetime = 1.0
+  vim.g.neovide_transparency = 0.9
   --
   --
 
