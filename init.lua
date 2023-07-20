56278cc12a859f1f6338826a165a4a8fe4cb765b
hylo <hylo@posteo.de>
2023-07-20 10:46:28 +0200

    feat: lualine shows path

--- init.lua
+++ init.lua
@@ -260,6 +260,13 @@ require('lazy').setup({
         component_separators = '',
         section_separators = '',
       },
+      sections = {
+        lualine_c = { { 'filename', path = 1 } },
+      },
+
+      inactive_sections = {
+        lualine_c = { { 'filename', path = 1 } },
+      },
     },
   },
 
